ff557f19110936afdb8d6592083f90ddf10a6055
a samad <h72313samad@gmail.com>
2019-12-14 09:22:52 +0330

    latest update

--- plate_detect/detect.py
+++ plate_detect/detect.py
@@ -1,66 +1,80 @@
 import cv2 as cv
 import sys
+import os
+import shutil
+import numpy as np
 
 
-model_addr = "model/frozen_inference_graph.pb"
-model_addr_txt = "model/graph.pbtxt"
-image_addr = sys.argv[1]
+def detect(img_addrs, out_folder):
+    model_addr = "model/frozen_inference_graph.pb"
+    model_addr_txt = "model/graph.pbtxt"
 
-LABELS = ["null","plate"]     
+    # LABELS = ["null","plate"]
+    cvNet = cv.dnn.readNetFromTensorflow(model_addr, model_addr_txt)
+    # --- pre process
+    imgs = [cv.imread(addr) for addr in img_addrs]
+    # img = cv.imread(image_addr)
+    for i in range(len(imgs)):
+        imgs[i] = cv.cvtColor(imgs[i], cv.COLOR_BGR2HSV)
+        clahe = cv.createCLAHE(clipLimit=4.0,tileGridSize=(4,4))
+        imgs[i][:, :, 2] = clahe.apply(imgs[i][:, :, 2])
+        imgs[i] = cv.cvtColor(imgs[i], cv.COLOR_HSV2BGR)
+    # img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    # img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+    # ---- feeding the net
+    cvOut = np.zeros((len(imgs), 1, 100, 7))
+    a = cv.dnn.blobFromImages(imgs, size=(300, 300), swapRB=True, crop=False)
+    for i in range(len(imgs)):
+        cvNet.setInput(a[i:i+1, :, :, :])
+        cvOut[i:i+1, :, :, :] = cvNet.forward()
+        print("item {} / {} Done!".format(i, len(imgs)))
 
-cvNet = cv.dnn.readNetFromTensorflow(model_addr, model_addr_txt)
-# --- pre process
-img = cv.imread(image_addr)
-img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-clahe = cv.createCLAHE(clipLimit=4.0,tileGridSize=(4,4))
-img[:, :, 2] = clahe.apply(img[:, :, 2])
-img = cv.cvtColor(img, cv.COLOR_HSV2BGR)
-# img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-# img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
-# ---- feeding the net
-rows = img.shape[0]
-cols = img.shape[1]
-cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
-cvOut = cvNet.forward()
+    if not os.path.exists(out_folder):
+        os.mkdir(out_folder)
+    else:
+        shutil.rmtree(out_folder)
+        os.mkdir(out_folder)
 
-# max_score = 0.5
-score_thr = 0.2
-max_area = 0
-print(cvOut.shape)
-for detection in cvOut[0, 0, :, :]:
-    score = float(detection[2])
-    if score > score_thr:
-        
-        label = "{}: {:.2f}%".format(LABELS[int(detection[1])], detection[2] * 100)
-        # print("[INFO] {}".format(label))
-        left = int(detection[3] * cols)
-        top = int(detection[4] * rows)
-        right = int(detection[5] * cols)
-        bottom = int(detection[6] * rows)
-        area = (bottom - top) * (right - left)
-        # --- detect of bounding box has some ratio and bigger area than others
-        if 0.1 < (bottom - top) / (right - left) < 0.4:
-            max_area = area
-            # --- if box was outside of image
-            left = 0 if left < 0 else left
-            top = 0 if top < 0 else top
-            right = img.shape[1] if right > img.shape[1] else right
-            bottom = img.shape[0] if bottom > img.shape[0] else bottom
-            # --- create a margin
-            left = left - 10 if left - 10 > 0 else left
-            top = top - 10 if top - 10 > 0 else top
-            right = right + 10 if right + 10 < img.shape[1] else right
-            bottom = bottom + 10 if bottom + 10 < img.shape[0] else bottom
-            
-            # for i in range(3):            
-            cv.imwrite(sys.argv[2], img[top:bottom, left:right])
-            # print("left:{} top:{} right:{} bottom:{}".format(left,top,right,bottom))
+    score_thr = 0.5
+    max_area = 0
 
-            cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
-#             left = left - 15 if left - 15 > 15 else left + 15
-#             top = top - 5
-#             cv.putText(img, label, (int(left), int(top)),
-#                 cv.FONT_HERSHEY_SIMPLEX, 0.5,(0,255,0), 2)
+    for i in range(cvOut.shape[0]):
+        for detection in cvOut[i, 0, :, :]:
+            score = float(detection[2])
+            label = int(detection[1])
+            if score > score_thr and label == 1:  # confident plate
+                rows = imgs[i].shape[0]
+                cols = imgs[i].shape[1]
+                left = int(detection[3] * cols)
+                top = int(detection[4] * rows)
+                right = int(detection[5] * cols)
+                bottom = int(detection[6] * rows)
+                area = (bottom - top) * (right - left)
+                # --- detect of bounding box has some ratio and bigger area than others
+                if 0.15 < (bottom - top) / (right - left) < 0.35:
+                    # max_area = area
+                    # --- if box was outside of image
+                    left = 0 if left < 0 else left
+                    top = 0 if top < 0 else top
+                    right = imgs[i].shape[1] if right > imgs[i].shape[1] else right
+                    bottom = imgs[i].shape[0] if bottom > imgs[i].shape[0] else bottom
+                    # --- create a margin
+                    left = left - 10 if left - 10 > 0 else left
+                    top = top - 10 if top - 10 > 0 else top
+                    right = right + 10 if right + 10 < imgs[i].shape[1] else right
+                    bottom = bottom + 10 if bottom + 10 < imgs[i].shape[0] else bottom
 
-cv.imwrite(sys.argv[2], img)
-        
+                    # for i in range(3):
+                    cv.imwrite(out_folder + '{}.png'.format(i), imgs[i][top:bottom, left:right])
+                    # print("left:{} top:{} right:{} bottom:{}".format(left,top,right,bottom))
+
+                    # cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+        #             left = left - 15 if left - 15 > 15 else left + 15
+        #             top = top - 5
+        #             cv.putText(img, label, (int(left), int(top)),
+        #                 cv.FONT_HERSHEY_SIMPLEX, 0.5,(0,255,0), 2)
+    #     cv.imwrite(sys.argv[2], img)
+
+if __name__ == "__main__":
+    img_folder = os.listdir(sys.argv[1])
+    out_folder = sys.argv[2]
--- runner.py
+++ runner.py
@@ -3,54 +3,56 @@ import sys
 from multiprocessing import Pool
 from time import perf_counter
 import shutil
+import plate_detect.detect as detector
 import numpy as np
 
 
-def f(x):
-    print("module:1 - img:{}".format(x[1]))
-    os.system("python3 detect.py ../{} output/{}.png".format(x[0], x[1]))
+def f(imgs):
+    # print("module:1 - img:{}".format(x[1]))
+    # os.system("python3 detect.py ../{} output/{}.png".format(x[0], x[1]))
+    # img_addrs = os.listdir("../{}".format(x[0]))
+    detector.detect(imgs, "output/")
 
 
-def f2(x):
-    print("module:3 - img:{}".format(x[0]))
-    os.system("python3 real_plates.py result/{}.jpg heat_maps/{}.jpg".format(x[0], x[1]))
+# def f2(x):
+#     print("module:3 - img:{}".format(x[0]))
+#     os.system("python3 real_plates.py result/{}.jpg heat_maps/{}.jpg".format(x[0], x[1]))
 
 
 if __name__ == '__main__':
     # ---- input files
     t0 = perf_counter()
-    a = ['test_set/images/' + addr for addr in os.listdir('test_set/images')]
-    b = ['test_set/Normal_plate_net/' + addr for addr in os.listdir('test_set/Normal_plate_net')]
-    c = ['test_set/siyasi/' + addr for addr in os.listdir('test_set/siyasi/')]
-    d = ['test_set/gozar_movaghat/' + addr for addr in os.listdir('test_set/gozar_movaghat/')]
+    a = ['../test_set/images/' + addr for addr in os.listdir('test_set/images')]
+    b = ['../test_set/Normal_plate_net/' + addr for addr in os.listdir('test_set/Normal_plate_net')]
+    c = ['../test_set/siyasi/' + addr for addr in os.listdir('test_set/siyasi/')]
+    d = ['../test_set/gozar_movaghat/' + addr for addr in os.listdir('test_set/gozar_movaghat/')]
     imgs = a + b + c + d
-
-    # imgs = [addr for i, addr in enumerate(a) if i > 0 and i % 2 == 0]
-
     # ---- module1 plate detection
     print("========= module1 =========")
     if os.path.exists('plate_detect/output'):
         shutil.rmtree('plate_detect/output')
         os.mkdir('plate_detect/output')
     os.chdir('plate_detect')
-    p = Pool(8)
-    jobs = zip(imgs, list(range(1, len(imgs) + 1)))
-    p.map(f, jobs)
+    f(imgs)
     os.chdir('../')
+    print()
+    # p = Pool(8)
+    # jobs = zip(imgs, list(range(1, len(imgs) + 1)))
+    # p.map(f, jobs)
 
     # ---- module2 OCR detection
     print("========= module2 =========")
-    # if os.path.exists('OCD/result/'):
-    #     shutil.rmtree('OCD/result/')
-    # os.mkdir('OCD/result/')
-    # if os.path.exists('OCD/plates/'):
-    #     shutil.rmtree('OCD/plates/')
-    # os.mkdir('OCD/plates/')
-    # if os.path.exists('OCD/heat_maps/'):
-    #     shutil.rmtree('OCD/heat_maps/')
-    # os.mkdir('OCD/heat_maps/')
-    # os.chdir("OCD/")
-    # os.system("python3 test.py --test_folder=../plate_detect/output")
+    if os.path.exists('OCD/result/'):
+        shutil.rmtree('OCD/result/')
+    os.mkdir('OCD/result/')
+    if os.path.exists('OCD/plates/'):
+        shutil.rmtree('OCD/plates/')
+    os.mkdir('OCD/plates/')
+    if os.path.exists('OCD/heat_maps/'):
+        shutil.rmtree('OCD/heat_maps/')
+    os.mkdir('OCD/heat_maps/')
+    os.chdir("OCD/")
+    os.system("python3 test.py --test_folder=../plate_detect/output")
 
     # ---- module3 post-process and OCR
     # f_list = sorted(os.listdir("result"))
